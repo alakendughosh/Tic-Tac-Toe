b743617cf7efd382465bba0a9a06d282b5514375
Alakendu Ghosh <alakendughosh421@gmail.com>
2025-08-23 08:05:03 +0530

    app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const winPatterns=[
     [0,4,8],
     [1,4,7],
     [2,5,8],
-    [2,5,6],
+    [2,4,6],
     [3,4,5],
     [6,7,8],
 ];
@@ -80,4 +80,5 @@ let checkWinner=()=>{
 };
 
 newgamebtn.addEventListener("click",resetGame);
+
 reset.addEventListener("click",resetGame);
